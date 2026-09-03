b783b3420544498d1aff79743bb3f471ec04e056
411-31-rodsarin <56306@yupparaj.ac.th>
2026-09-03 13:35:56 +0700

    vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -108,7 +108,7 @@ ans3 = st.text_input(
 ans4 = st.text_input(
     "ข้อ 4: Can I borrow your `p e _ _ i l`?. ✏️",
     value=st.session_state.ans4_val,
-
+)
     
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
